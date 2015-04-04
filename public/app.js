a0295456006ca119e76448759adc225c78df1bf5
Wenson Tsai <wensontsai@gmail.com>
2015-04-04 17:18:55 -0400

    bower dependencies sorted

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
+angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute',, 'mgcrea.ngStrap', 'ui.bootstrap'])
 
   .config(function(){
 
