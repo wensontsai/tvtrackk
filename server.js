e202994a45c9719244c08318bf342baaedf71b58
Wenson Tsai <wensontsai@gmail.com>
2015-04-11 15:54:46 -0400

    progressing

--- server.js
+++ server.js
@@ -3,17 +3,113 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 
 var app = express();
-
 app.set('port', process.env.PORT || 3030);
 
+
+// db schemas //
+var showSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  airsDayOfWeek: String,
+  airsTime: String,
+  firstAired: Date,
+  genre: [String],
+  network: String,
+  overview: String,
+  rating: Number,
+  ratingCount: Number,
+  status: String,
+  poster: String,
+  subscribers: [{
+    type: mongoose.Schema.Types.ObjectId, ref: 'User'
+  }],
+  episodes: [{
+    season: Number,
+    espisodeNumber: Number,
+    episodeName: String,
+    firstAired: Date,
+    overview: String
+  }]
+});
+
+
+
+var userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function(err, hash){
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+
+
+var User = mongoose.model('User', userSchema);
+var Show = mongoose.model('Show', showSchema);
+
+
+
+
+mongoose.connect('localhost');
+
+
+// Middleware //
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Routes //
+app.get('/api/shows', function(req, res, next){
+  var query = Show.find();
+  if(req.query.genre){
+    query.where({ genre: req.query.genre });
+  } else if (req.query.alphabet){
+    query.where({ name: new RegExp('^' + '[' + req.query.alphabet + ']', 'i') })
+  } else {
+    query.limit(12);
+  }
+  query.exec(function(err, shows){
+    if (err) return next(err);
+    res.send(shows);
+  });
+});
+
+app.get('/api/shows/:id', function(req, res, next){
+  Show.findById(req.params.id, function(err, show){
+    if (err) return next(err);
+    res.send(show);
+  });
+});
+
+
+// error middleware, prints stack trace to console, but returns only error message to user //
+
+
+// server //
 app.listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
 });
